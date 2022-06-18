ff169b20a6871b4cc96d9a442dfa7a8b4d1f582a
whathebea <bea.graciast@gmail.com>
2022-06-18 18:40:49 -0300

    added helloHandler function

--- simple-web-server/main.go
+++ simple-web-server/main.go
@@ -1,11 +1,34 @@
 package main
 
-import(
+import (
 	"fmt"
 	"log"
 	"net/http"
 )
 
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404.\n Page not found", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "Method not supported", http.StatusNotFound)
+		return
+	}
+	fmt.Fprintf(w, "hello")
+}
 func main() {
-	
+	fileServer := http.FileServer(http.Dir("./static")) //checking the static folder
+	http.Handle("/", fileServer)
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/hello", helloHandler)
+	// all these functions are from the http package imported previously
+
+	fmt.Printf("Server started on port 8080\n")
+
+	//error handling
+	//nil is value unintialized (=0)
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
 }
