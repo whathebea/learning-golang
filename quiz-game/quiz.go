f8bf50f9a6edfc7b3928a1318ca9bfbc8470c1e7
whathebea <bea.graciast@gmail.com>
2022-06-28 22:22:17 -0300

    starting todo

--- quiz-game/quiz.go
+++ quiz-game/quiz.go
@@ -6,8 +6,29 @@ func main() {
 	fmt.Println("Hey! What's your name?")
 
 	var name string
-	
+
 	fmt.Scan(&name)
 
 	fmt.Printf("Hey %v, let's test your knowledge with a quiz!", name)
+
+	fmt.Printf("\nInside of which HTML element do we put javascript? ")
+
+	var answer string
+	fmt.Scan(&answer)
+
+	if answer == "<script>" {
+		fmt.Println("You're right!")
+	} else {
+		fmt.Println("You're wrong :C")
+	}
+
+	fmt.Printf("\n Next! Where is the correct place to insert JavaScript? ")
+	var answer2 string
+	fmt.Scan(&answer2)
+
+	if answer2 == "<body>" {
+		fmt.Println("You're right!")
+	} else {
+		fmt.Println("You're wrong :C")
+	}
 }
---
+++ todo-app/todo.go
@@ -0,0 +1,48 @@
+package todo
+
+import (
+	"errors"
+	"time"
+)
+
+// creating structure for each todo
+type item struct {
+	Task        string
+	Done        bool
+	CreatedAt   time.Time
+	CompletedAt time.Time
+}
+
+type Todos []item
+
+func (t *Todos) Add(task string) {
+	todo := item{
+		Task:        task,
+		Done:        false,
+		CreatedAt:   time.Now(),
+		CompletedAt: time.Time{},
+	}
+
+	*t = append(*t, todo)
+}
+
+func (t *Todos) Complete(index int) error {
+	ls := *t
+	if index <= 0 || index  > len(ls) {
+		return errors.New("invalid index")
+	}
+
+	ls[index-1].CompletedAt = time.Now()
+	ls[index-1].Done = true
+
+	return nil
+}
+
+func (t *Todos) Delete(index int) error {
+	ls := *t
+	if index <= 0 || index  > len(ls) {
+		return errors.New("invalid index")
+	}
+
+	*l = append(ls[:index-1])
+}
