3d2735f4485244586f35f9679af3416b0a10b0b9
whathebea <bea.graciast@gmail.com>
2022-06-29 19:21:19 -0300

    importing error

--- todo-app/cmd/todo/main.go
+++ todo-app/cmd/todo/main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	".../todo"
+)
+
+const (
+	todoFile = ".todo.json"
+)
 
 func main() {
-	fmt.Println("testing")
+	add := flag.Bool("add", false, "add task")
+
+	flag.Parse()
+
+	todos := &todo.Todos{}
+
+	if err := todos.Load(todoFile); err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+
+	switch {
+	case *add:
+		todos.Add("sample task")
+		err := todos.Store(todoFile)
+
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+	default:
+		fmt.Fprintln(os.Stdout, "no task")
+		os.Exit(0)
+	}
 }
