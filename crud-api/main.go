45bc0ac30b01b9066f1349ea4385ce65c9eb0e44
whathebea <bea.graciast@gmail.com>
2022-06-30 22:07:08 -0300

    added mock data

--- crud-api/main.go
+++ crud-api/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
 	"github.com/gorilla/mux"
 )
 
@@ -23,7 +24,13 @@ type Author struct {
 var books []Book
 
 func main() {
+	// Init Router
 	r := mux.NewRouter()
+	// Mock Data
+	books = append(books, Book{ID: "1", Isbn: "12345", Title: "Book One", Author: &Author{Firstname: "John", Lastname: "Doe"}, Year: 2022})
+	books = append(books, Book{ID: "2", Isbn: "54321", Title: "Book Two", Author: &Author{Firstname: "Steve", Lastname: "Smith"}, Year: 2021})
+
+	// Route Handlers / Endpoints
 	r.HandleFunc("/books", getBooks).Methods("GET")
 	r.HandleFunc("/books/{id}", getBook).Methods("GET")
 	r.HandleFunc("/books", createBook).Methods("POST")
