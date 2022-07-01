548b4315fee17fc7016848cababa83e2bd2bdf33
whathebea <bea.graciast@gmail.com>
2022-06-30 22:00:55 -0300

    added routes

--- crud-api/main.go
+++ crud-api/main.go
@@ -1,27 +1,35 @@
 package main
 
-import(
+import (
 	"fmt"
 	"log"
-	"encoding/json"
-	"math/rand"
 	"net/http"
-	"strconv"
 	"github.com/gorilla/mux"
 )
 
-type Book struct{
-	ID string `json:"id"`
-	Isbn string `json:"isbn"`
-	Title string `json:"title"`
+type Book struct {
+	ID     string  `json:"id"`
+	Isbn   string  `json:"isbn"`
+	Title  string  `json:"title"`
 	Author *Author `json:"author"`
-	Year int `json:"year"`
+	Year   int     `json:"year"`
 }
 
 type Author struct {
 	Firstname string `json:"firstname"`
-	Lastname string `json:"lastname"`
+	Lastname  string `json:"lastname"`
 }
 
 var books []Book
 
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/books", getBooks).Methods("GET")
+	r.HandleFunc("/books/{id}", getBook).Methods("GET")
+	r.HandleFunc("/books", createBook).Methods("POST")
+	r.HandleFunc("/books/{id}", updateBook).Methods("PUT")
+	r.HandleFunc("/books/{id}", deleteBook).Methods("DELETE")
+
+	fmt.Println("Server is running on port 8000")
+	log.Fatal(http.ListenAndServe(":8000", r))
+}
