0469fe7bbfaadd440f1632a534df94832d00d478
whathebea <bea.graciast@gmail.com>
2022-06-30 22:39:35 -0300

    added functions

--- crud-api/main.go
+++ crud-api/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
-
+	"strconv"
 	"github.com/gorilla/mux"
 )
 
@@ -23,6 +25,72 @@ type Author struct {
 
 var books []Book
 
+// Get all books
+func getBooks(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(books)
+}
+
+// Delete a book
+func deleteBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	// Loop through books and find one with the id from the params
+	for index, book := range books {
+		if book.ID == params["id"] {
+			books = append(books[:index], books[index+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(books)
+}
+
+// Update a book
+func updateBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	// Loop through books and find one with the id from the params
+	for index, book := range books {
+		// If the id from the params matches the id in the book, update the book
+		if book.ID == params["id"] {
+			var bookUpdate Book
+			_ = json.NewDecoder(r.Body).Decode(&bookUpdate)
+			books = append(books[:index], books[index+1:]...)
+			books = append(books, bookUpdate)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(books)
+}
+
+// Get a single book
+func getBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	// Loop through books and find one with the id from the params
+	for _, book := range books {
+		// If the id from the params matches the id in the book, return the book
+		if book.ID == params["id"] {
+			// Return the book
+			json.NewEncoder(w).Encode(book)
+			return
+		}
+	}
+}
+
+// Create a new book
+func createBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var book Book
+	_ = json.NewDecoder(r.Body).Decode(&book)
+	// Add ID
+	book.ID = strconv.Itoa(rand.Intn(10000000))
+	// Add book to books array
+	books = append(books, book)
+	// Return the book
+	json.NewEncoder(w).Encode(book)
+}
+
 func main() {
 	// Init Router
 	r := mux.NewRouter()
@@ -37,6 +105,7 @@ func main() {
 	r.HandleFunc("/books/{id}", updateBook).Methods("PUT")
 	r.HandleFunc("/books/{id}", deleteBook).Methods("DELETE")
 
+	// Start Server
 	fmt.Println("Server is running on port 8000")
 	log.Fatal(http.ListenAndServe(":8000", r))
 }
